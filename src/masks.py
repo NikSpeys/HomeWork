f1ca690d764c208899b2f337eea11522b7bc7e41
Nikita Potapov <nekit132465@list.ru>
2025-02-09 18:59:32 +0300

    fixes inaccuracies

--- src/masks.py
+++ src/masks.py
@@ -1,12 +1,11 @@
 def get_mask_card_number(card_number: str) -> str:
     """Функция маскировки номера банковской карты"""
     if len(card_number) == 16 and card_number.isdigit():
-
         return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[12:]}"
     return "Номер карты введён не корректно"
 
 
-#print(get_mask_card_number("7000792289606361"))
+# print(get_mask_card_number("7000792289606361"))
 
 
 def get_mask_account(mask_account: str) -> str:
