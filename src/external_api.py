901f34f009a7a853d92a90c59dd7468dbb59d49d
Nikita Potapov <nekit132465@list.ru>
2025-03-11 21:07:52 +0300

    all fixes

--- src/external_api.py
+++ src/external_api.py
@@ -7,16 +7,24 @@ load_dotenv()
 
 API_KEY = os.getenv("API_KEY")
 
+if not API_KEY:
+    raise ValueError("API_KEY not found. Please check your .env file.")
+
 
 def get_amount(transaction: dict) -> float:
     """Функция принимает на вход транзакцию, конвертирует сумму в рубли и возвращает сумму транзакции"""
     amount = transaction["operationAmount"]["amount"]
     code = transaction["operationAmount"]["currency"]["code"]
-    if transaction["operationAmount"]["currency"]["code"] != "RUB":
+
+    if code != "RUB":
         response = requests.get(
-            f"https://api.apilayer.com/exchangerates_data/convert?to={"RUB"}&from={code}&amount={amount}",
-            headers={"apikey": f"{API_KEY}"},
+            f"https://api.apilayer.com/exchangerates_data/convert?to=RUB&from={code}&amount={amount}",
+            headers={"apikey": API_KEY},
         )
+
+        if response.status_code != 200:
+            raise Exception(f"Error fetching data from API: {response.status_code} - {response.text}")
+
         result = response.json()
         return result["result"]
     else:
